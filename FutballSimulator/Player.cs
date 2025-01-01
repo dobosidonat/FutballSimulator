a82b704da011bafc7230c3d4786b31efc23ad61e
dobosidonat <dobosidonat@stud.uni-obuda.hu>
2025-01-01 18:10:16 +0100

    Játékos osztály létrehozása

--- FutballSimulator/Player.cs
+++ FutballSimulator/Player.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace FutballSimulator
 {
+    /// <summary>
+    /// Játékos osztáy
+    /// </summary>
     public class Player
     {
+        public string Name { get; set; }    //Játékos neve
+        public string Position { get; set; } //Játékos kora
+        public int Age { get; set; }    //Játékos kora
+        public double Rating { get; set; }  //Játékos értékelése
+        public double MarketValue { get; set; } //Játékos piaci értéke
+
+        public override string ToString()
+        {
+            return $"{Name} ({Position}) - Rating: {Rating}, Value: {MarketValue:C}";
+        }
     }
 }
