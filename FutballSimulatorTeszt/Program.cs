e59ea77b04c7007e25c36d97bf91ef7c8b9b2a74
dobosidonat <dobosidonat@stud.uni-obuda.hu>
2025-01-01 19:06:47 +0100

    Tesztfájl létrehozása, dll és tesztféjl összekapcsolása

---
+++ FutballSimulatorTeszt/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using FutballSimulator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Fehérvár FC játékosok betöltése fájlból
+        var fehervarPlayers = FileHandler.LoadPlayersFromFile("fehervar_players.txt");
+
+        // Fehérvár FC csapat létrehozása
+        var fehervar = new Team
+        {
+            Name = "Fehérvár FC",
+            Budget = 2000000,
+            Players = fehervarPlayers
+        };
+
+        Console.WriteLine("Fehérvár FC kezdő állapota:");
+        Console.WriteLine(fehervar);
+
+        // Átigazolási piac betöltése
+        var transferMarket = FileHandler.LoadPlayersFromFile("transfer_market.txt");
+
+        // Optimalizált igazolások
+        var bestTransfers = TransferOptimizer.OptimizeTransfers(transferMarket, fehervar.Budget);
+        foreach (var player in bestTransfers)
+        {
+            fehervar.AddPlayer(player);
+        }
+
+        Console.WriteLine("\nFehérvár FC állapota az igazolások után:");
+        Console.WriteLine(fehervar);
+
+        Console.WriteLine("\nIgazolt játékosok:");
+        foreach (var player in bestTransfers)
+        {
+            Console.WriteLine(player);
+        }
+    }
+}
