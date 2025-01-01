cd95ddac15ecabaec3551f869d38ef4bdd1c6ff7
dobosidonat <dobosidonat@stud.uni-obuda.hu>
2025-01-01 18:26:51 +0100

    Csapat osztály megadása

--- FutballSimulator/Player.cs
+++ FutballSimulator/Player.cs
@@ -12,11 +12,16 @@ namespace FutballSimulator
     public class Player
     {
         public string Name { get; set; }    //Játékos neve
-        public string Position { get; set; } //Játékos kora
+        public string Position { get; set; } //Játékos pozíciója
         public int Age { get; set; }    //Játékos kora
-        public double Rating { get; set; }  //Játékos értékelése
+        public double Rating { get; set; }  //Játékos értékelése (0-100)
         public double MarketValue { get; set; } //Játékos piaci értéke
 
+
+        /// <summary>
+        /// A játékos adatait tartalmazó szöveges reprezentációt ad vissza.
+        /// </summary>
+        /// <returns>Szöveg, amely tartalmazza a játékos adatait.</returns>
         public override string ToString()
         {
             return $"{Name} ({Position}) - Rating: {Rating}, Value: {MarketValue:C}";
---
+++ FutballSimulator/Team.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FutballSimulator
+{
+    //Csapat osztály
+    public class Team
+    {
+        
+
+        public string Name { get; set; } // A csapat neve
+        public double Budget { get; set; } // A csapat költségvetése
+        public List<Player> Players { get; set; } = new List<Player>(); // A csapat játékosainak listája
+
+        /// <summary>
+        /// A csapat játékosainak átlagos értékelését számítja ki.
+        /// </summary>
+        public double AverageRating => Players.Count > 0 
+            ? Players.Average(p => p.Rating) 
+            : 0;
+
+        /// <summary>
+        /// Játékos hozzáadása a csapathoz és a költségvetés frissítése.
+        /// </summary>
+        /// <param name="player">A hozzáadandó játékos.</param>
+        public void AddPlayer(Player player)
+        {
+            Players.Add(player);
+            Budget -= player.MarketValue;
+            Console.WriteLine($"{player.Name} automatikusan leigazolva a {Name} csapatába!");
+        }
+
+        /// <summary>
+        /// A csapat adatait tartalmazó szöveges reprezentációt ad vissza.
+        /// </summary>
+        /// <returns>Szöveg, amely tartalmazza a csapat adatait.</returns>
+        public override string ToString()
+        {
+            return $"{Name} - Költségvetés: {Budget:C}, Átlagos Értékelés: {AverageRating:F1}";
+        }
+    }
+}
